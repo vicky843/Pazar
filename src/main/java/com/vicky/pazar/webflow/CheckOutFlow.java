73d94b9928b86ea59133546e2225fdb8643fe3f9
K.Saiteja <ksaiteja844@gmail.com>
2016-12-16 00:34:23 +0530

    Got many errors and need to fix it.

--- src/main/java/com/vicky/pazar/webflow/CheckOutFlow.java
+++ src/main/java/com/vicky/pazar/webflow/CheckOutFlow.java
@@ -1,9 +1,10 @@
 package com.vicky.pazar.webflow;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.google.gson.Gson;
 import com.vicky.pazar.model.Ordermodel;
+import com.vicky.pazar.model.Shippingmodel;
 
 @Component
 public class CheckOutFlow {
@@ -13,4 +14,13 @@ public class CheckOutFlow {
 		Ordermodel orders=new Ordermodel();
 		return orders;
 	}
+	/*public String addshipdetails(Ordermodel order,Shippingmodel shipaddress )
+	{
+		Gson gson=new Gson();
+		String shipaddressng=gson.toJson(shipaddress);
+		System.out.println("this is shipng"+shipaddress);
+order.setShippingaddress(shipaddressng);
+		return "buyingprocess";
+		
+	}*/
 }
